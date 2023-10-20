bae57ec6783e4f981e7534911e37155a5fbcefec
Daniel Carbajal <edudani81@gmail.com>
2023-10-20 01:14:23 -0600

    New search implemented by capital, country name and region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,23 @@ export class CountriesService {
     constructor(private httpClient: HttpClient) { }
 
     searchCapital(term: string): Observable<Country[]> {
-        return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+        return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+            .pipe(
+                catchError( () => of([]) ),
+            );
+    }
+
+    searchCountry(term: string): Observable<Country[]> {
+        return this.httpClient.get<Country[]>(`${this.apiUrl}/name/${term}`)
+            .pipe(
+                catchError( () => of([]) )
+            );
+    }
+    
+    searchByRegion(region: string): Observable<Country[]> {
+        return this.httpClient.get<Country[]>(`${this.apiUrl}/region/${region}`)
+            .pipe(
+                catchError( () => of([]) )
+            );
     }
 }
