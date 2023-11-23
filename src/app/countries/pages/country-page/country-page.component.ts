57a3c0a28eda6945450f080e95fa7429cbb320fa
Daniel Carbajal <edudani81@gmail.com>
2023-11-23 00:27:06 -0600

    End of chapter

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  public country?: Country;
+  constructor(private activatedRoute: ActivatedRoute, private countriesService: CountriesService, private router: Router) {}
 
+  //Primer implementación con params, permanece como referencia nada más
+  // ngOnInit(): void {
+  //     this.activatedRoute.params
+  //       .subscribe(params => {
+  //         console.log({ params: params['id'] }); //Se usa con los corchetes porque el objeto Params funciona de esa manera por el tipado estricto
+  //       })
+  // }
+  
+  //Método totalmente extraño con subscribes anidados
+  ngOnInit(): void {
+      this.activatedRoute.params
+        .pipe(
+          switchMap( ({ id: code }) => this.countriesService.searchCountryByAlphaCode( code ) )
+        )
+        .subscribe( (country) => {
+          if (!country) {
+            return this.router.navigateByUrl('');
+          }
+          console.log({ country });
+          return this.country = country;
+        })
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, count, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
     private apiUrl: string = "https://restcountries.com/v3.1";
     constructor(private httpClient: HttpClient) { }
+
+    searchCountryByAlphaCode(alphaCode: string): Observable<Country | null> {
+        return this.httpClient.get<Country[]>(`${this.apiUrl}/alpha/${alphaCode}`)
+            .pipe(
+                map( countries => countries.length > 0 ? countries[0] : null ),
+                catchError( () => of(null) )
+            );
+    }
 
     searchCapital(term: string): Observable<Country[]> {
         return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${term}`)
